fc02d7246df73bf232ed755ce6eabb151b10eb3c
zariuz <nechetov@hotmail.com>
2020-08-04 00:25:59 +0300

    added cartItem

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+
 import Button from './../Button/index';
 
 const PizzaBlock = ({
